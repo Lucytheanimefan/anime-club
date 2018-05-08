0c132254d8eaa275cea48cbf0cd03d8012f29f94
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-05-07 20:15:26 -0400

    Store high score anime, update score if new user also rated that anime highly

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -18,19 +18,34 @@ module.exports.topanime = async function(callback) {
 
       //var completed_anime_reqs = 0;
       for (let j = 0; j < result.length; j++) {
-      	//completed_anime_reqs += 1;
+        //completed_anime_reqs += 1;
         let anime = result[j];
         let mal_id = anime['anime_id'];
         let title = anime['anime_title'];
         let score = anime['score'];
-        animes.update_one(mal_id, { mal_id: mal_id, title: title, average_score: score }, function(err) {
-          if (err) {
+        if (score >= 8) {
+          const findAnimeQuery = animes.find_by_mal_id(mal_id);
+          var updateAnimeQuery;
+          findAnimeQuery.then(function(anime) {
+            if (anime && anime.users != null && !anime.users.includes(users[i].mal_username) ) {
+            	console.log(anime.users);
+            	console.log('Update score: ' + title);
+              updateAnimeQuery = animes.update_score(anime._id, score, users[i].mal_username);
+            } else if (!anime){
+            	console.log('Create new anime');
+              updateAnimeQuery = Anime.create({ mal_id: mal_id, title: title, average_score: score, scores: [score], users: [users[i].mal_username] });
+              //updateAnimeQuery = animes.update_one(mal_id, { mal_id: mal_id, title: title, average_score: score });
+            }
+            else {
+            	console.log('Do nothing');
+            }
+            return updateAnimeQuery;
+          }).then(function(updatedAnime) {
+          }).catch(function(err) {
             console.log(err);
-          }
-        });
-        // console.log('Found anime:')
-        console.log(anime);
-
+            return callback(err);
+          })
+        }
       }
 
       if (completed_requests == users.length) {
--- models/anime.js
+++ models/anime.js
@@ -6,23 +6,25 @@ var AnimeSchema = new mongoose.Schema({
   mal_id: {
     type: Number,
     required: true,
+    unique: true
   },
   title: {
     type: String,
     required: true
   },
   users: [{
-    type: mongoose.Schema.ObjectId,
-    ref: 'User'
+    type: String, // mal_username
   }],
   average_score: {
-    type:Number
+    type: Number,
   },
+  scores: [{
+    type: Number,
+  }],
   description: {
-    type:String
+    type: String
   }
-})
+});
 
-
-var Anime = mongoose.model('Anime', AnimeSchema);
+const Anime = mongoose.model('Anime', AnimeSchema);
 module.exports = Anime;
